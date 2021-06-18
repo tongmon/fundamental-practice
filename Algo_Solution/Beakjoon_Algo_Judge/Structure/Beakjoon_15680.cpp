e71859040691ae5fd6b7ee9b33d6becccfa71ae0
tongstar <tongmon@hanmail.net>
2021-06-19 08:56:48 +0900

    Solution

---
+++ Algo_Solution/Beakjoon_Algo_Judge/Structure/Beakjoon_15680.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+https://www.acmicpc.net/problem/15680
+그냥 출력 문제.
+*/
+
+int main() 
+{
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int N;
+	cin >> N;
+	cout << (N ? "Leading the Way to the Future" : "YONSEI");
+}
