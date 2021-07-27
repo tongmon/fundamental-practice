681c20e008c604b7668be67582aa84e20bcc2b30
tongstar <tongmon@hanmail.net>
2021-07-27 22:32:45 +0900

    Solution

--- Algo_Solution/Beakjoon_Algo_Judge/Recursion/Beakjoon_9184.cpp
+++ Algo_Solution/Beakjoon_Algo_Judge/Recursion/Beakjoon_9184.cpp
@@ -3,37 +3,46 @@ using namespace std;
 
 /*
 https://www.acmicpc.net/problem/9184
+재귀호출 줄이기 위해 DP를 사용하는 문제다.
+맵을 이용하여 DP를 구현했다.
+재귀시 전역 변수 사용을 주의해야 한다.
+잘 못 사용하면 반환 값이 이상해질 수 있다.
+이 문제를 통해 구현속도를 늘려간다.
 */
 
-int a, b, c, A, B, C, D;
-map<tuple<int, int, int>, int> M;
+using tup = tuple<int, int, int>;
+tup R[] = { {0,0,-1},{0,-1,-1},{0,-1,0},{-1,0,0},{-1,-1,0},{-1,0,-1},{-1,-1,-1},{20,20,20} };
+map<tup, int> M;
 
-int func(int i, int j, int k) {
-	if (i <= 0 || j <= 0 || k <= 0)
+int func(tup K) {
+	int A[4], a = get<0>(K), b = get<1>(K), c = get<2>(K);
+	if (a <= 0 || b <= 0 || c <= 0)
 		return 1;
-	if (i > 20 || j > 20 || k > 20) {
-		M[{ 20, 20, 20 }] = A = M.find({ 20,20,20 }) != M.end() ? M[{ 20, 20, 20 }] : func(20, 20, 20);
-		return A;
+	if (a > 20 || b > 20 || c > 20) {
+		M[R[7]] = A[0] = M.find(R[7]) != M.end() ? M[R[7]] : func(R[7]);
+		return A[0];
 	}
-	if (i < j && j < k) {
-		M[{i, j, k - 1}] = A = M.find({ i,j,k - 1 }) == M.end() ? func(i, j, k - 1) : M[{i, j, k - 1}];
-		M[{i, j - 1, k - 1}] = B = M.find({ i,j - 1,k - 1 }) == M.end() ? func(i, j - 1, k - 1) : M[{i, j - 1, k - 1}];
-		M[{i, j - 1, k}] = C = M.find({ i,j - 1,k }) == M.end() ? func(i, j - 1, k) : M[{i, j - 1, k}];
-		return A + B - C;
+	if (a < b && b < c) {
+		for (int i = 0; i < 3; i++) {
+			tup B = { a + get<0>(R[i]),b + get<1>(R[i]),c + get<2>(R[i]) };
+			M[B] = A[i] = M.find(B) == M.end() ? func(B) : M[B];
+		}
+		return A[0] + A[1] - A[2];
 	}
-	M[{i - 1, j, k}] = A = M.find({ i - 1,j,k }) == M.end() ? func(i - 1, j, k) : M[{i - 1, j, k}];
-	M[{i - 1, j - 1, k}] = B = M.find({ i - 1,j - 1,k }) == M.end() ? func(i - 1, j - 1, k) : M[{i - 1, j - 1, k}];
-	M[{i - 1, j, k - 1}] = C = M.find({ i - 1,j,k - 1 }) == M.end() ? func(i - 1, j, k - 1) : M[{i - 1, j, k - 1}];
-	M[{i - 1, j - 1, k - 1}] = D = M.find({ i - 1,j - 1,k - 1 }) == M.end() ? func(i - 1, j - 1, k - 1) : M[{i - 1, j - 1, k - 1}];
-	return A + B + C - D;
+	for (int i = 3; i < 7; i++) {
+		tup B = { a + get<0>(R[i]),b + get<1>(R[i]),c + get<2>(R[i]) };
+		M[B] = A[i - 3] = M.find(B) == M.end() ? func(B) : M[B];
+	}
+	return A[0] + A[1] + A[2] - A[3];
 }
 
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int a, b, c;
 	cin >> a >> b >> c;
 	while (!(a == -1 && b == -1 && c == -1)) {
-		cout << "w(" << a << ", " << b << ", " << c << ") = " << func(a, b, c) << '\n';
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << func({ a, b, c }) << '\n';
 		cin >> a >> b >> c;
 	}
 }
