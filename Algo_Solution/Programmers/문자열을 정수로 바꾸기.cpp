65397fde24957446e876619a0784724fb0277bba
tongstar <tongmon@hanmail.net>
2021-10-09 23:16:04 +0900

    Solution

---
+++ Algo_Solution/Programmers/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+https://programmers.co.kr/learn/courses/30/lessons/12925
+문자열을 정수로 바꾸기 문제.
+stl이 해결해준다... 가족 여행 땜시 간단한 문제로 때운다.(사실 핑계...)
+*/
+
+int solution(string s) {
+    return stoi(s);
+}
