d3d30755020913f7eb2dac841cfe1222a8fb5d6a
tongstar <68733249+tongmon@users.noreply.github.com>
2022-01-25 16:59:24 +0900

    22/01/25 Study

--- C#/WPF/DarkTheme_UI/Wpf_Practice/MVVM/ViewModel/MainViewModel.cs
+++ C#/WPF/DarkTheme_UI/Wpf_Practice/MVVM/ViewModel/MainViewModel.cs
@@ -5,6 +5,24 @@ namespace Wpf_Practice.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public HomeViewModel HomeVm { get; set; }
 
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
+        }
     }
 }
