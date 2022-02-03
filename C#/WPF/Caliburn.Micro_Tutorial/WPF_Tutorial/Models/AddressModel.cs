c9824cc8a5cf4b7864e2a51e461dbe3ca2d30275
tongstar <68733249+tongmon@users.noreply.github.com>
2022-02-03 15:32:18 +0900

    22/02/03 Study

---
+++ C#/WPF/Caliburn.Micro_Tutorial/WPF_Tutorial/Models/AddressModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Tutorial.Models
+{
+    public class AddressModel
+    {
+        public int AddressId { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        public string FullAddress
+        {
+            get
+            {
+                return $"{StreetAddress}, {City}, {State}, {ZipCode}";
+            }
+        }
+    }
+}
--- C#/WPF/Caliburn.Micro_Tutorial/WPF_Tutorial/Models/PersonModel.cs
+++ C#/WPF/Caliburn.Micro_Tutorial/WPF_Tutorial/Models/PersonModel.cs
@@ -9,7 +9,18 @@ namespace WPF_Tutorial.Models
 {
     public class PersonModel
     {
+        public int PersonId { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsAlive { get; set; }
+        public decimal AccountBalance { get; set; }
+        public List<AddressModel> Addresses { get; set; } = new List<AddressModel>();
+        public AddressModel PrimaryAddress { get; set; }
+        public string FullName
+        {
+            get { return FirstName + ' ' + LastName; }
+        }
     }
 }
