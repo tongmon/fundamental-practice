397f6889c0f881e2a00eae30c25ae08ecb8d1513
tongstar <tongmon@hanmail.net>
2022-02-14 18:45:57 +0900

    Solution

---
+++ Algo_Solution/Beakjoon_Algo_Judge/String/Beakjoon_10926.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+https://www.acmicpc.net/problem/10926
+그저... 문자열 더하기
+*/
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Console.ReadLine() + "??!");
+    }
+}
