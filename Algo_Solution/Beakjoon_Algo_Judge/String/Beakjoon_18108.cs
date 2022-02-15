1019dc44a3f29969b10f6cf7ca6970f9771801b5
tongstar <tongmon@hanmail.net>
2022-02-15 20:38:05 +0900

    Solution

---
+++ Algo_Solution/Beakjoon_Algo_Judge/String/Beakjoon_18108.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+https://www.acmicpc.net/problem/18108
+그저... 빼기... 한번쯤 쉬어가도 되나...ㅠ
+*/
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(int.Parse(Console.ReadLine()) - 543);
+    }
+}
