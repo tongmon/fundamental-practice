e42829dda1bb645f8324c2ea8c6b60aa09dd5901
tongstar <tongmon@hanmail.net>
2022-12-18 22:43:33 +0900

    catch2 update

--- Programming_Language/Cpp/CMake/Catch2Sample/App/main.cpp
+++ Programming_Language/Cpp/CMake/Catch2Sample/App/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
+#include <mylib.h>
 #include <vector>
 
-#include <catch.hpp>
-#include <mylib.h>
-
 int main() {
+
     return 0;
 }
--- Programming_Language/Cpp/CMake/Catch2Sample/Lib/mylib.cpp
+++ Programming_Language/Cpp/CMake/Catch2Sample/Lib/mylib.cpp
@@ -1,3 +1,5 @@
+#include <mylib.h>
+
 unsigned int factorial(unsigned int n) {
     return n <= 1 ? n : n * factorial(n - 1);
 }
