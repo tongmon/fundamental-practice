5ce006f64967026a538a805cbb100e7857f6c241
tongstar <68733249+tongmon@users.noreply.github.com>
2024-04-15 17:34:51 +0900

    Update playwright proj

--- Programming Language/JavaScript/PlaywrightProjects/PlaywrightAutomation/playwright.config.js
+++ Programming Language/JavaScript/PlaywrightProjects/PlaywrightAutomation/playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require("@playwright/test");
 
 /**
  * Read environment variables from file.
@@ -11,41 +11,77 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
+  // 테스트 수행할 코드가 들어있는 폴더 위치
+  testDir: "./tests",
+
+  // 테스트를 병렬 수행할 것인지 여부
   fullyParallel: true,
+
+  // 테스트 진행 중 timeout에 기재된 시간 동안 아무런 반응이 없다면 실패처리함
+  // 즉 해당 시간은 test 명령에 대한 시간 제한 값임
+  timeout: 30 * 1000,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  /* 테스트의 결과를 보고 하기위한 파일의 형식, 자세한 내용은 https://playwright.dev/docs/test-reporters 링크 참조 */
+  reporter: "html",
+
+  expect: {
+    // expect 한 라인 수행하는 경우, 최대 timeout에 기재된 시간을 기다림
+    // 즉 해당 시간은 expect 명령에 대한 시간 제한 값임
+    timeout: 5000,
+  },
+
+  /* 테스트 코드가 수행될 때 공유되는 특성들, 자세한 것은 https://playwright.dev/docs/api/class-testoptions 참조. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
+
+    // 수행할 브라우저 유형
+    browserName: "chromium",
+
+    launchOptions: {
+      channel: "msedge",
+    },
+
+    // true -> 브라우저를 띄우지 않고 행함, false -> 브라우저를 띄우고 행함
+    headless: false,
   },
 
-  /* Configure projects for major browsers */
+  /* 해당 섹션에 적혀있는 브라우저들 마다 테스트가 진행됨 */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+    //{
+    //  name: "chromium",
+    //  use: {
+    //    channel: "chrome",
+    //    //...devices["Desktop Chrome"],
+    //  },
+    //},
+
+    //{
+    //  name: "firefox",
+    //  use: { ...devices["Desktop Firefox"] },
+    //},
+
+    //{
+    //  name: "webkit",
+    //  use: { ...devices["Desktop Safari"] },
+    //},
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "edge",
+      use: {
+        channel: "msedge",
+      },
     },
 
     /* Test against mobile viewports. */
@@ -76,4 +112,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
--- Programming Language/JavaScript/PlaywrightProjects/PlaywrightAutomation/tests/UIBasicstest.spec.js
+++ Programming Language/JavaScript/PlaywrightProjects/PlaywrightAutomation/tests/UIBasicstest.spec.js
@@ -1,6 +1,115 @@
-const { test } = require("@playwright/test");
+﻿// 프로젝트에 playwright을 설치하려면 밑의 명령어를 수행한다.
+// npm init playwright@latest
+//
+// playwright을 수행하는 경우 playwright 전용 browser를 이용하는 것이 좋다. 밑 명령어로 playwright browser를 설치한다.
+// npx playwright install
+//
+// playwright 프로젝트 수행을 위해선 밑과 같은 명령어들을 사용할 수 있다.
+// npx playwright test -> playwright.config.js를 찾아 해당 파일에서 지시한대로 테스트 수행
+// npx playwright test --project=chromium -> chromium으로 테스트 수행
+// npx playwright test tests/UIBasicstest.spec.js // 특정 파일을 지정해 테스트 수행, 나머지 설정은 playwright.config.js에 지정된 대로 가져감
+// npx playwright test --debug -> 디버그 모드로 테스트 수행
+//
+// 모든 테스트는 headless 방식으로 진행되기 때문에 별도의 명령어가 없다면 브라우저가 보이지 않음
+// npx playwright test --headed
+// 위와 같이 headed 옵션을 달아줘야 실제로 브라우저 진행 상황이 보임, 혹은 playwright.config.js의 headless 옵션을 이용
+//
+// 리포트 결과를 보는 명령어는 밑과 같다.
+// npx playwright show-report
 
-// 함수를 전달할 때 async로 전달해야만 await가 의미있다.
-test("First Playwright test", async () => {
+// test와 expect 키워드를 이용하기 위해 선언함
+const { test, expect } = require("@playwright/test");
+
+// browser를 넘기는 경우는 무조건 {}로 감싸야 playwright의 browser로 인식한다.
+// 그리고 함수를 전달할 때 async로 전달해야만 await가 의미있다.
+test("First Playwright test", async ({ browser }) => {
+  // 쿠키나 예전에 깔았던 크로니움 기반 확장들을 모두 날린 상태의 순수한 세션을 만듦
+  // 순수한 세션을 만들고 이용하여 속도를 높이는 효과도 있음
+  // 생성되는 브라우저의 종류는 playwright.config.js에서 설정가능
+  const context = await browser.newContext();
+
+  // 새로운 탭 생성
+  const page = await context.newPage();
+
+  // 특정 링크로 이동
+  await page.goto("https://google.com/");
+});
+
+// browser 말고 page를 넘기면
+// const context = await browser.newContext();
+// const page = await context.newPage();
+// 위 코드를 생략할 수 있음
+// { browser, page }에서 browser가 필요없다면 { page } 이렇게 넘겨도 됨
+test("Second Playwright test", async ({ browser, page }) => {
   await page.goto("https://playwright.dev/");
+
+  // 해당 페이지 타이틀 획득
+  console.log(await page.title());
+
+  // expect는 테스트를 하기 위한 주요 함수다.
+  // toHaveTitle: 특정 페이지의 타이틀을 비교하여 똑같으면 테스트 패스, 아니면 실패로 간주함
+  await expect(page).toHaveTitle(
+    "Fast and reliable end-to-end testing for modern web apps | Playwright"
+  );
+});
+
+// test.only를 이용하면 해당 테스트는 수행되지만 나머지 정의된 모든 테스트는 스킵된다.
+// test.only("Skip all the tests below this test", async ({ browser, page }) => {
+//   await page.goto("https://naver.com/");
+// });
+
+test("Third Playwright test", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  // locator는 웹 페이지의 특정 UI를 찾는 곳에 쓰임
+  // locator를 통해 특정 UI를 찾는 방법에는 4가지가 있음
+  //
+  // 1. ID가 주어진 경우
+  // tagname#id 혹은 #id (ex.input#username)
+  //
+  // 2. 클래스가 주어진 경우
+  // tagname.class 혹은.class (ex.input.form-control)
+  //
+  // 3. Css Attribute를 통해 찾는 경우
+  // 해당 방식의 장점은 정규식 사용이 가능하다는 것이다.
+  // 자세한 내용은 https://developer.mozilla.org/ko/docs/Learn/CSS/Building_blocks/Selectors/Attribute_selectors 참조
+  // [attribute = 'value'] (ex. [name = 'username'])
+  //
+  // 4. Css 부모 자식 관계를 통해 찾는 경우 (>> 혹은 공백 둘 다 가능)
+  // parent_tagname >> child_tagname (ex.form >> div >> input)
+  //
+  // SelectorsHub라는 브라우저 확장앱을 설치하면 더 편하다.
+  await page.locator("#username").fill("tongstar"); // fill 함수로 input UI를 채움
+  await page.locator("[type='password']").fill("my password");
+  await page.locator("#signInBtn").click(); // 특정 버튼 클릭
+
+  const alertText = await page.locator("[style*='block']");
+  console.log(await alertText.textContent()); // 텍스트 추출
+
+  await expect(alertText).toContainText("Incorrect"); // Incorrect 텍스트를 획득하는지 확인
+});
+
+test.only("Fourth Playwright test", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  const idInput = page.locator("#username");
+  const pwInput = page.locator("[type='password']");
+  const signInBtn = page.locator("#signInBtn");
+
+  await idInput.fill("rahulshettyacademy"); // fill 함수로 input UI를 채움
+  await pwInput.fill("learning");
+  await signInBtn.click(); // 특정 버튼 클릭
+
+  const cardInfo = page.locator(".card-body a");
+
+  // locator가 탐색하여 찾은 element가 여럿인 경우 nth(index)를 이용하면 된다.
+  console.log(await cardInfo.nth(0).textContent());
+
+  // 밑도 nth(0)과 같은 기능을 수행함
+  // console.log(await cardInfo.first().textContent());
+
+  // allTextContents를 통해 전체 텍스트를 획득함
+  // allTextContents 함수는 화면 렌더링이 완료되는 시점까지 기다려주지 않는다
+  //
+  console.log(await cardInfo.allTextContents());
 });
