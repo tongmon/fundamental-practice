67264e8707e51bf2f6cf85304cee35da3627d0fe
tongstar <68733249+tongmon@users.noreply.github.com>
2025-01-10 23:47:10 +0900

    Update react study

--- Programming Language/JavaScript/React/004. state_and_hook/src/App.jsx
+++ Programming Language/JavaScript/React/004. state_and_hook/src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { CoreConcepts } from "./components/CoreConcepts";
 import { Header } from "./components/Header";
 import { TabButton } from "./components/TabButton";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 
 // index.jsx를 보면 알겠지만 밑 App() 컴포넌트는 단 한 번만 렌더링된다.
 // 하지만 특정 상황에서는 App() 렌더링을 여러 번 수행해야 하는데 이때 상태(state)와 훅(Hook)을 사용한다.
@@ -12,7 +12,8 @@ function App() {
   // 밑과 같이 Hook은 컴포넌트 최상위에서만 호출해야 한다.
   // 컴포넌트 밖에서나 handleSelect과 같은 컴포넌트 내부 함수에서 호출하면 안된다.
   // useState 함수는 배열을 반환하며 첫 번째 요소는 상태 값이고 두 번째 요소는 상태 값을 변경하는 리액트 함수이다.
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  // selectedTopic의 초기값은 "components"이다.
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
     // 상태 값을 변경하는 리액트 함수를 호출하여 상태 값을 변경한다.
@@ -52,7 +53,13 @@ function App() {
             // 밑과 같이 selectedTopic 값이 변경되면 useState 훅에 의해 App() 함수가 재호출되어 렌더링이 다시 수행된다.
             // 따라서 특정 버튼 클릭 시 handleSelect() 함수 내부에서 selectedTopic 값이 변경되어 해당 내용이 실시간 반영된다.
           }
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
