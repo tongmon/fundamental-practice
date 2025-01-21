c79ec322b1d637350635535376107eaa06707546
tongstar <68733249+tongmon@users.noreply.github.com>
2025-01-21 23:52:39 +0900

    Update react study

--- Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/App.jsx
+++ Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
             // 당연하게도 밑의 두 Player 컴포넌트는 서로 공유되는 값이 없이 독립적으로 동작한다.
             // 따라서 두 컴포넌트는 서로 영향을 주지 않고 코드가 꼬일 일이 없다.
           }
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
       </div>
     </main>
--- Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/components/Player.jsx
+++ Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/components/Player.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 
-export function Player({ name, symbol }) {
+export function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName); // 플레이어 이름을 기억할 이벤트
   const [isEditing, setIsEditing] = useState(false);
+
+  // input 태그의 onChange 이벤트에서는 event 객체를 인자로 받아올 수 있다.
+  // 따라서 onChange 이벤트에 넘겨줄 함수 포인터는 이벤트 객체를 받아와 활용이 가능하다.
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
   function handleEditClick() {
     // 밑 방식은 isEditing의 값을 실시간으로 변경 및 반영하지 않기에 React에서 권장하는 방식이 아니다.
@@ -14,15 +21,20 @@ export function Player({ name, symbol }) {
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      // 밑은 양방향 바인딩의 예시로 input 필드에 데이터가 입력될 때마다 onChange 이벤트가 발생하고
+      // 해당 이벤트 핸들러인 handleChange 함수가 호출되어 playerName이 변경되고 이렇게 변경된 playerName이 input 필드에 반영된다.
+      // 즉 입력이 바뀔 때마다 playerName이 변경되고 playerName이 변경될 때마다 input 필드에 반영되는 양방향 바인딩이다.
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
