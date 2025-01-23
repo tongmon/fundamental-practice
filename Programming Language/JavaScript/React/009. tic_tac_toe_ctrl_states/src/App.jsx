cc5a002ca18fb410a46f30284822b8f6b3b6a555
tongstar <68733249+tongmon@users.noreply.github.com>
2025-01-23 22:27:13 +0900

    Update react study

--- Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/App.jsx
+++ Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/App.jsx
@@ -1,4 +1,5 @@
 import { Player } from "./components/Player";
+import { GameBoard } from "./components/GameBoard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
