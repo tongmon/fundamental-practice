0e3f619892c26a98e114e9e4a1c7968906873837
tongstar <68733249+tongmon@users.noreply.github.com>
2025-01-28 22:38:47 +0900

    Update React study

--- Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/App.jsx
+++ Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/App.jsx
@@ -11,12 +11,27 @@ function App() {
   const [activePlayer, setActivePlayer] = useState("X");
 
   // 게임 보드를 클릭할 때마다 플레이어를 교체하는 함수
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((prevActivePlayer) => {
       return prevActivePlayer === "X" ? "O" : "X";
     });
 
-    setGameTurns((prevGameTurns) => {});
+    setGameTurns((prevGameTurns) => {
+      // activePlayer 상태를 사용하지 않는 이유는 이 함수가 호출될 때 이미 activePlayer 상태가 변경되었을 수도 있기 때문이다.
+      // setActivePlayer 함수로 인해 React 내부적으로 상태 변경이 이루어지는데 해당 변경이 정확히 언제 이루어지는지 알 수 없기 때문에
+      // 밑과 같은 방식으로 현재 플레이 상태를 획득해야 한다.
+      let currentPlayer = "X";
+      if (prevGameTurns.length > 0 && prevGameTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevGameTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -38,12 +53,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/components/GameBoard.jsx
+++ Programming Language/JavaScript/React/009. tic_tac_toe_ctrl_states/src/components/GameBoard.jsx
@@ -1,29 +1,36 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      // prevGameBoard은 initialGameBoard를 참조하기에 해당 배열을 바로 변경하게 되면 prevGameBoard를 이미 내부적으로 상태 변경 시점에서 사용하고 있는 React와 충돌하여 문제가 발생할 수 있다.
-      // 따라서 initialGameBoard의 복사본을 만들어서 변경해야 문제가 확실히 발생하지 않는다.
-      // 밑은 이차원 배열을 깊은 복사하고 있다.
-      // 깊은 복사이기에 그 속까지 '...' 연산자를 이용해 복사해주고 있다.
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-    onSelectSquare();
+    gameBoard[row][col] = player;
   }
+
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  //function handleSelectSquare(rowIndex, colIndex) {
+  //  setGameBoard((prevGameBoard) => {
+  //    // prevGameBoard은 initialGameBoard를 참조하기에 해당 배열을 바로 변경하게 되면 prevGameBoard를 이미 내부적으로 상태 변경 시점에서 사용하고 있는 React와 충돌하여 문제가 발생할 수 있다.
+  //    // 따라서 initialGameBoard의 복사본을 만들어서 변경해야 문제가 확실히 발생하지 않는다.
+  //    // 밑은 이차원 배열을 깊은 복사하고 있다.
+  //    // 깊은 복사이기에 그 속까지 '...' 연산자를 이용해 복사해주고 있다.
+  //    const updatedBoard = [
+  //      ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //    ];
+  //    updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //    return updatedBoard;
+  //  });
+  //
+  //  onSelectSquare();
+  //}
 
   return (
     <ol id="game-board">
@@ -32,7 +39,7 @@ export function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
