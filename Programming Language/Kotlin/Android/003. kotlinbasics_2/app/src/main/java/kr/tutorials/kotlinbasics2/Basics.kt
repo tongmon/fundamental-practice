e3e1cd766989e5d2f48069c42541550247b470ae
tongstar <68733249+tongmon@users.noreply.github.com>
2025-02-25 23:55:58 +0900

    Update android study

--- Programming Language/Kotlin/Android/003. kotlinbasics_2/app/src/main/java/kr/tutorials/kotlinbasics2/Basics.kt
+++ Programming Language/Kotlin/Android/003. kotlinbasics_2/app/src/main/java/kr/tutorials/kotlinbasics2/Basics.kt
@@ -1,12 +1,44 @@
 package kr.tutorials.kotlinbasics2
 
+// 밑과 같이 자료만 들어있는 클래스도 생성이 가능하다.
+// C++의 변수만 있는 구조체라고 보면 될 듯.
+data class DataStructure(val someData1: String,
+                         val someData2: Int)
+
 fun main() {
     SomeFunc(100)
     println(SomeFuncWithRet("Hello"))
 
-    var someDog = Dog("tongstar")
+    val someDog = Dog("tongstar")
     someDog.Bark("tongmon")
     println("someDog.name = ${someDog.name}")
+
+    var someData = DataStructure("Data 01", 100)
+    
+    // 고정 크기 배열은 밑과 같이 만듦
+    val someList = listOf<Int>(1, 2, 3, 4, 5)
+
+    // 동적 배열은 밑과 같음
+    val someMutableList = mutableListOf<String>("hi", "hello", "bye", "farewell")
+    someMutableList.add("good bye")
+    someMutableList.remove("bye")
+    println(someMutableList)
+
+    val hasGoodBye = someMutableList.contains("good bye")
+    if (hasGoodBye){
+        for (item in someMutableList){
+            println("$item\n")
+        }
+        for ((index, item) in someMutableList.withIndex()) {
+            println("$item\n")
+        }
+        for (index in someMutableList.indices) {
+            println("${someMutableList[index]}\n")
+        }
+        for (index in 0 until someMutableList.size){
+            println("${someMutableList[index]}\n")
+        }
+    }
 }
 
 fun SomeFunc(someArg: Int)
--- Programming Language/Kotlin/Android/003. kotlinbasics_2/app/src/main/java/kr/tutorials/kotlinbasics2/Dog.kt
+++ Programming Language/Kotlin/Android/003. kotlinbasics_2/app/src/main/java/kr/tutorials/kotlinbasics2/Dog.kt
@@ -5,6 +5,7 @@ package kr.tutorials.kotlinbasics2
 // class Dog (var name: String) 이렇게 var를 달아주면 name은 멤버 변수가 됨, 따로 클래스 내부에 멤버 변수를 선언하지 않아도 되어 코드량을 줄일 수 있음
 class Dog (var name: String){
     var someMemberVariable: Boolean = true
+    private var somePrivateVariable: Int = 100 // private 변수
 
     // 생성자
     init {
