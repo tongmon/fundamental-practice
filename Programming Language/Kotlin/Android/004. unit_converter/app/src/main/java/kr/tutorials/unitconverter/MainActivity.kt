0811b0b51e8ece3091970404c8b555ee2cf2214c
tongstar <68733249+tongmon@users.noreply.github.com>
2025-02-27 23:47:59 +0900

    Update android study

--- Programming Language/Kotlin/Android/004. unit_converter/app/src/main/java/kr/tutorials/unitconverter/MainActivity.kt
+++ Programming Language/Kotlin/Android/004. unit_converter/app/src/main/java/kr/tutorials/unitconverter/MainActivity.kt
@@ -11,17 +11,23 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
@@ -52,11 +58,17 @@ fun UnitConverter(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Unit Converter")
+        // modifier = Modifier.padding(100.dp) 코드를 사용해도 UI 간격 조절이 가능하다.
+        Text(text = "Unit Converter" /*, modifier = Modifier.padding(100.dp)*/)
+        // Spacer를 통해 UI간 간격을 조절할 수 있음
+        // dp는 화면 상대적인 단위라 화면 크기 제약이 없어서 좋다.
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {
         })
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
             // 복잡한 레이아웃을 설계할 때 Box가 사용된다.
+            // Box안에 같이 Button과 DropdownMenu를 위치시킴으로 두 UI가 같은 context를 공유하게 되고 상호작용이 가능하다.
             Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select")
@@ -65,17 +77,56 @@ fun UnitConverter(modifier: Modifier = Modifier) {
                         contentDescription = "Arrow Down"
                     )
                 }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Millimeters") },
+                        onClick = { }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters") },
+                        onClick = { }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters") },
+                        onClick = { }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet") },
+                        onClick = { }
+                    )
+                }
             }
+            Spacer(modifier = Modifier.width(16.dp))
             Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select")
                     Icon(
                         Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down"
+                    )
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Millimeters") },
+                        onClick = { }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters") },
+                        onClick = { }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters") },
+                        onClick = { }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet") },
+                        onClick = { }
                     )
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Result:")
     }
 
     // Column(modifier = modifier) {
