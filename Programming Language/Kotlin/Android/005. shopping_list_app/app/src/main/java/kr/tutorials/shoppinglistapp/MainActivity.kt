f5d663fbd91a061066bde572cc8bc2abab0b1ea7
tongstar <68733249+tongmon@users.noreply.github.com>
2025-03-04 23:23:18 +0900

    Update android study

---
+++ Programming Language/Kotlin/Android/005. shopping_list_app/app/src/main/java/kr/tutorials/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package kr.tutorials.shoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.tutorials.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ) {
+                            Text("Add Item")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
