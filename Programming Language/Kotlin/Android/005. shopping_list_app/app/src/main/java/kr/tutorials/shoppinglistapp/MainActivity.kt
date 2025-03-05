d0d2bc45b0ac5918c60d9276a9a7a59ef3d26c99
tongstar <68733249+tongmon@users.noreply.github.com>
2025-03-05 23:33:04 +0900

    Update android study

--- Programming Language/Kotlin/Android/005. shopping_list_app/app/src/main/java/kr/tutorials/shoppinglistapp/MainActivity.kt
+++ Programming Language/Kotlin/Android/005. shopping_list_app/app/src/main/java/kr/tutorials/shoppinglistapp/MainActivity.kt
@@ -8,13 +8,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.tutorials.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +31,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // ShoppingItem이 여러개 들어갈 수 있는 상태 변수 sItems 배열을 선언한다.
+                    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
                     Column(
                         modifier = Modifier
                             .padding(innerPadding)
@@ -36,9 +45,28 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text("Add Item")
                         }
+                        // LazyColumn은 동적 배열 UI를 만들 때 사용된다.
+                        // 예를 들어 인스타 그램 페이지를 밑으로 무한정 내릴 수 있는 그런 상황에서 사용될 수 있다.
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            // 해당 LazyColumn과 연동되는 상태 변수 아이템 묶음은 밑과 같이 연결할 수 있다.
+                            items(sItems) {
+
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
+)
