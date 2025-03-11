97743fa939872191b2d192534b597d78721e909d
tongstar <68733249+tongmon@users.noreply.github.com>
2025-03-11 23:41:32 +0900

    Update android study

--- Programming Language/Kotlin/Android/005. shopping_list_app/app/src/main/java/kr/tutorials/shoppinglistapp/ShoppingList.kt
+++ Programming Language/Kotlin/Android/005. shopping_list_app/app/src/main/java/kr/tutorials/shoppinglistapp/ShoppingList.kt
@@ -12,8 +12,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -153,5 +158,14 @@ fun ShoppingListItem(
             )
     ) {
         Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row(modifier = Modifier.padding(8.dp)) {
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
     }
 }
