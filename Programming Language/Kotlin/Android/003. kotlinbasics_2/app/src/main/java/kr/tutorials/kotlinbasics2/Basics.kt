a7dbfb35b898062ce6605f00114a18f81ce5d22e
tongstar <68733249+tongmon@users.noreply.github.com>
2025-03-23 20:58:39 +0900

    Update android study

--- Programming Language/Kotlin/Android/003. kotlinbasics_2/app/src/main/java/kr/tutorials/kotlinbasics2/Basics.kt
+++ Programming Language/Kotlin/Android/003. kotlinbasics_2/app/src/main/java/kr/tutorials/kotlinbasics2/Basics.kt
@@ -14,6 +14,8 @@ fun main() {
     println("someDog.name = ${someDog.name}")
 
     var someData = DataStructure("Data 01", 100)
+    // 밑과 같이 참조 말고 특정 원소만 변경하여 복사를 수행할 수 있다.
+    var otherData = someData.copy(someData2 = 200)
     
     // 고정 크기 배열은 밑과 같이 만듦
     val someList = listOf<Int>(1, 2, 3)
@@ -43,6 +45,19 @@ fun main() {
     // 밑과 같이 js에서 배열을 순회하는 map 함수가 코틀린에도 존재한다.
     // doubled는 2, 4, 6이 될 것이다.
     val doubled = someList.map { it * 2 }
+
+    // String? 이렇게 뒤에 '?'가 붙으면 nullable 자료형이 된다.
+    // null이거나 String이거나 둘 중 하나라는 것이다.
+    var name: String? = "Ella"
+    // let으로 블록을 지정하여 불필요한 변수 선언을 피할 수 있다.
+    // it 변수는 원본 객체의 복사본이다.
+    // let 블록 내의 마지막 라인은 let 블록의 return 값이 된다.
+    var letRet = name?.let {
+        println(it.uppercase())
+        "This is \"let\" block!"
+    }
+    // letRet은 "This is "let" block!"가 된다.
+    println(letRet)
 }
 
 fun SomeFunc(someArg: Int)
