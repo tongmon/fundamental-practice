3e30af8a0461f1e0d6d7e07b2981108ceba6f380
tongstar <68733249+tongmon@users.noreply.github.com>
2025-04-17 23:13:28 +0900

    Update react study

--- Programming Language/JavaScript/React/014. tailwindcss/src/components/Header.jsx
+++ Programming Language/JavaScript/React/014. tailwindcss/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className="flex flex-col items-center mt-8 mb-16">
       <img src={logo} alt="A canvas" className='object-contain mb-8 w-44 h-44' />
       <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
     </header>
   );
 }
